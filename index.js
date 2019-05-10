ed13dc1b52d390d45cbb0085e3ba27cf6ef5d1b9
Wilson <wiyu98@gmail.com>
2019-05-10 15:37:24 -0400

    Smaller picture sizes

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ window.onload = function(e){
 
 function changeTextSize(size){
   let fontsizechangers = document.getElementsByClassName("fontsizechange");
-  
+  let images = document.getElementsByTagName("IMG");
+
   for(var i = 0; i < fontsizechangers.length; i++){
     fontsizechangers[i].classList.remove("active");
   }
 
   if (size == 1){
     document.getElementsByTagName("BODY")[0].style.fontSize = "0.85rem";
+    for (var i = 0; i < images.length; i++){
+      images[i].style.maxWidth = "75%";
+    }
   }
 
   if (size == 2){
     document.getElementsByTagName("BODY")[0].style.fontSize = "1rem";
+    for (var i = 0; i < images.length; i++){
+      images[i].style.maxWidth = "80%";
+    }
   }
 
   if (size == 3){
     document.getElementsByTagName("BODY")[0].style.fontSize = "1.2rem";
+    for (var i = 0; i < images.length; i++){
+      images[i].style.maxWidth = "90%";
+    }
   }
 
   if (size == 4){
     document.getElementsByTagName("BODY")[0].style.fontSize = "1.5rem";
+    for (var i = 0; i < images.length; i++){
+      images[i].style.maxWidth = "100%";
+    }
   }
 
   fontsizechangers[size - 1].classList.add("active");
--- setTextSize.js
+++ setTextSize.js
@@ -1,14 +1,14 @@
 if (sessionStorage.textSize != null) {
   if (sessionStorage.textSize == 1) {
-      document.write("<style>body { font-size: 0.85em; }</style>");
+      document.write("<style>body { font-size: 0.85em; } img { max-width: 75%;}</style>");
   }
   if (sessionStorage.textSize == 2) {
-      document.write("<style>body { font-size: 1em; }</style>");
+      document.write("<style>body { font-size: 1em; } img { max-width: 80%;}</style>");
   }
   if (sessionStorage.textSize == 3) {
-      document.write("<style>body { font-size: 1.2em; }</style>");
+      document.write("<style>body { font-size: 1.2em; } img { max-width: 90%;}</style>");
   }
   if (sessionStorage.textSize == 4) {
-      document.write("<style>body { font-size: 1.5em; }</style>");
+      document.write("<style>body { font-size: 1.5em; } img { max-width: 100%;}</style>");
   }
 }
